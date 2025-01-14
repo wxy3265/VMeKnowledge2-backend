099a2026595aaad00e23cf168c86706fdddfc98d
wxy3265 <546668033@qq.com>
2025-01-14 23:54:21 +0800

    Create Project.

---
+++ src/main/java/com/vmeknowledge/VMeKnowledge2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.vmeknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMeKnowledge2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VMeKnowledge2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vmeknowledge/VMeKnowledge2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmeknowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VMeKnowledge2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
