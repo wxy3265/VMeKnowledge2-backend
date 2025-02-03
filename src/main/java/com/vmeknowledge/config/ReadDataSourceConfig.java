81070ac8e8dde9435c051656c374753aa4c424f3
wxy3265 <546668033@qq.com>
2025-02-03 17:03:54 +0800

    add logs

--- src/main/java/com/vmeknowledge/config/ReadDataSourceConfig.java
+++ src/main/java/com/vmeknowledge/config/ReadDataSourceConfig.java
@@ -1,11 +1,13 @@
 package com.vmeknowledge.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
+@Slf4j
 @Configuration
 public class ReadDataSourceConfig {
 
@@ -19,11 +21,14 @@ public class ReadDataSourceConfig {
         if (dryRun == null) {
             dryRun = "0";
         }
+        String url = "";
         if (!dryRun.equals("0")) {
-            dataSource.setUrl("jdbc:mysql://127.0.0.1:300" + dryRun + "/vmeknowledge");
+            url = "jdbc:mysql://127.0.0.1:300" + dryRun + "/vmeknowledge";
         } else {
-            dataSource.setUrl("jdbc:mysql://127.0.0.1:3000/vmeknowledge");
+            url = "jdbc:mysql://127.0.0.1:3000/vmeknowledge";
         }
+        log.info("[MySQL] Running at :{}", url);
+        dataSource.setUrl(url);
         return dataSource;
     }
 
