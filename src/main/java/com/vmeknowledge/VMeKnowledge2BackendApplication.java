04f6fbf87a9bac8b5c0fdd3e6a667cdf8cd446cd
wxy3265 <546668033@qq.com>
2025-02-03 19:32:04 +0800

    add logs

--- src/main/java/com/vmeknowledge/VMeKnowledge2BackendApplication.java
+++ src/main/java/com/vmeknowledge/VMeKnowledge2BackendApplication.java
@@ -1,10 +1,16 @@
 package com.vmeknowledge;
 
 import com.vmeknowledge.initializer.Initializer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
 @SpringBootApplication
 public class VMeKnowledge2BackendApplication {
 
@@ -12,6 +18,20 @@ public class VMeKnowledge2BackendApplication {
         ApplicationContext context = SpringApplication.run(VMeKnowledge2BackendApplication.class, args);
         Initializer initializer = context.getBean(Initializer.class);
         initializer.init();
+        String url, dryRun = System.getenv("DRY_RUN");
+        if (!dryRun.equals("0")) {
+//            url = "jdbc:mysql://mysql-test" + dryRun + ":300" + dryRun + "/vmeknowledge";
+            url = "jdbc:mysql://mysql-test" + dryRun + ":3306/vmeknowledge";
+        } else {
+            url = "jdbc:mysql://mysql-online:3000/vmeknowledge";
+        }
+        try {
+            Connection connection = DriverManager.getConnection(url, "root", "31321");
+            log.debug("连接成功！");
+        } catch (SQLException e) {
+            log.error("连接失败！");
+            throw new RuntimeException(e);
+        }
     }
 
 }
