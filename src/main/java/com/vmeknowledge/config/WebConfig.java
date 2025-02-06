093cc46b190011e6907dfe0ac3b50dac981f88d8
Map1e <3412883097@qq.com>
2025-02-06 14:03:25 +0800

    完成了请求次数拦截器，一分钟内如果单个ip发送超过100条请求就会返回429状态码显示"Too many requests"

--- src/main/java/com/vmeknowledge/config/WebConfig.java
+++ src/main/java/com/vmeknowledge/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.vmeknowledge.config;
 
 import com.vmeknowledge.interceptor.LoginCheckInterceptor;
+import com.vmeknowledge.interceptor.RateLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,8 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private LoginCheckInterceptor loginCheckinterceptor;
 
+    @Autowired
+    private RateLimitInterceptor rateLimitInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginCheckinterceptor).addPathPatterns("/**");
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/register","/login"); // 配置需要限流的接口路径
+        registry.addInterceptor(loginCheckinterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/error");
     }
 }
