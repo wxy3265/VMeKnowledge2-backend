beae274a39acb652f6897fd139304ffbc84008b4
Map1e <3412883097@qq.com>
2025-02-07 17:32:43 +0800

    修改了id的格式，使用mongodb自己生成的id

--- src/main/java/com/vmeknowledge/mapper/KnowledgeMapper.java
+++ src/main/java/com/vmeknowledge/mapper/KnowledgeMapper.java
@@ -3,5 +3,5 @@ package com.vmeknowledge.mapper;
 import com.vmeknowledge.pojo.Knowledge;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface KnowledgeMapper extends MongoRepository<Knowledge,Integer> {
+public interface KnowledgeMapper extends MongoRepository<Knowledge,Object> {
 }
