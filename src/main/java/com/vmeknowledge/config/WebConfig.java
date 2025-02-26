f813838f0e722d1100ed0d1055b7e78f50e449da
Eastwood3265 <546668033@qq.com>
2025-02-26 16:47:31 +0800

    Merged request interceptor (#11)
    Co-authored-by: Map1e <3412883097@qq.com>

--- src/main/java/com/vmeknowledge/config/WebConfig.java
+++ src/main/java/com/vmeknowledge/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.vmeknowledge.config;
 
 import com.vmeknowledge.interceptor.LoginCheckInterceptor;
+import com.vmeknowledge.interceptor.RateLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,8 +13,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private LoginCheckInterceptor loginCheckinterceptor;
 
+    @Autowired
+    private RateLimitInterceptor rateLimitInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor).addPathPatterns("/**");
         registry.addInterceptor(loginCheckinterceptor).addPathPatterns("/**");
     }
 }
