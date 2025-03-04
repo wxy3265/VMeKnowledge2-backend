e79acbdc4a8cf01f1f9567a4b46c975f35ff4694
Map1e <3412883097@qq.com>
2025-03-04 09:14:36 +0800

    新建知识搜索分支

--- src/main/java/com/vmeknowledge/mapper/KnowledgeMapper.java
+++ src/main/java/com/vmeknowledge/mapper/KnowledgeMapper.java
@@ -2,7 +2,6 @@ package com.vmeknowledge.mapper;
 
 import com.vmeknowledge.pojo.Knowledge;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import java.util.List;
 
 public interface KnowledgeMapper extends MongoRepository<Knowledge,Object> {
